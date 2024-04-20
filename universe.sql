46b4e4ce67143fab1186689e59e617fc89436964
Mayko Costa <maykopr@hotmail.com>
2024-04-20 14:34:32 +0000

    prenchido os dados fictícios das tabelas galaxy,planet, e star

--- universe.sql
+++ universe.sql
@@ -88,7 +88,7 @@ CREATE TABLE public.galaxy (
     galaxy_id integer NOT NULL,
     name character varying(20) NOT NULL,
     type character varying(20),
-    mass_sm integer,
+    mass_sm bigint,
     diameter_light_years integer
 );
 
@@ -125,7 +125,7 @@ CREATE TABLE public.moon (
     moon_id integer NOT NULL,
     name character varying(20) NOT NULL,
     planet character varying(20),
-    mass numeric,
+    diameter_km numeric,
     distance_from_planet numeric,
     planet_id integer
 );
@@ -166,7 +166,8 @@ CREATE TABLE public.planet (
     mass numeric,
     distance_from_sun_au numeric,
     has_moon boolean,
-    star_id integer
+    star_id integer,
+    has_life boolean
 );
 
 
@@ -271,65 +272,106 @@ ALTER TABLE ONLY public.star ALTER COLUMN star_id SET DEFAULT nextval('public.st
 -- Data for Name: constellation; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
+INSERT INTO public.constellation VALUES (1, 'orion', 'betelgeuse', 'via lactea', 'norte');
+INSERT INTO public.constellation VALUES (2, 'cruzeiro do sul', 'acrux, mimosa', 'via lactea', 'sul');
+INSERT INTO public.constellation VALUES (3, 'ursa maior', 'dubhe, merak', 'via lactea', 'norte');
+INSERT INTO public.constellation VALUES (4, 'escorpiao', 'antares', 'via lactea', 'sul');
+INSERT INTO public.constellation VALUES (5, 'dragon', 'aldibah', 'andromeda', 'norte');
 
 
 --
 -- Data for Name: galaxy; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
+INSERT INTO public.galaxy VALUES (1, 'via lactea', 'espiral', 1000000000000, 1000000);
+INSERT INTO public.galaxy VALUES (2, 'andromeda', 'espiral', 1500000000000, 120000);
+INSERT INTO public.galaxy VALUES (3, 'triangulo', 'espiral', 200000000000, 60000);
+INSERT INTO public.galaxy VALUES (4, 'cigarro', 'irregular', 50000000000, 30000);
+INSERT INTO public.galaxy VALUES (5, 'centaurus a', 'eliptica', 100000000000, 60000);
+INSERT INTO public.galaxy VALUES (6, 'rodamoinho', 'espiral', 160000000000, 60000);
+INSERT INTO public.galaxy VALUES (7, 'bode', 'espiral', 30000000000, 10000);
+INSERT INTO public.galaxy VALUES (8, 'messier 87', 'eliptica', 2000000000000, 12000);
+INSERT INTO public.galaxy VALUES (9, 'sombrero', 'espiral', 800000000000, 50000);
+INSERT INTO public.galaxy VALUES (10, 'catavento', 'espiral', 100000000000, 70000);
 
 
 --
 -- Data for Name: moon; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
+INSERT INTO public.moon VALUES (2, 'lua', 'terra', 3.474, 384400, 1);
+INSERT INTO public.moon VALUES (3, 'europa', 'jupiter', 3.122, 671034, 3);
+INSERT INTO public.moon VALUES (4, 'tita', 'saturno', 5.151, 1221870, 4);
+INSERT INTO public.moon VALUES (5, 'tritao', 'netuno', 2.707, 354800, 5);
+INSERT INTO public.moon VALUES (6, 'io', 'jupiter', 3.643, 421700, 3);
+INSERT INTO public.moon VALUES (7, 'encelado', 'saturno', 504, 237948, 4);
+INSERT INTO public.moon VALUES (8, 'ganimedes', 'jupiter', 5.268, 1882700, 3);
+INSERT INTO public.moon VALUES (9, 'calisto', 'jupiter', 4.820, 1882700, 3);
+INSERT INTO public.moon VALUES (10, 'miranda', 'urano', 471.6, 129900, 6);
 
 
 --
 -- Data for Name: planet; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
+INSERT INTO public.planet VALUES (1, 'terra', 'terrestre', 1, 1, true, 1, true);
+INSERT INTO public.planet VALUES (2, 'marte', 'terrestre', 0.11, 1.5, true, 1, false);
+INSERT INTO public.planet VALUES (3, 'jupiter', 'gasoso', 317.8, 5.2, true, 1, false);
+INSERT INTO public.planet VALUES (4, 'saturno', 'gasoso', 95.2, 9.5, true, 1, false);
+INSERT INTO public.planet VALUES (5, 'netuno', 'gasoso', 17.15, 30.07, true, 1, false);
+INSERT INTO public.planet VALUES (6, 'urano', 'gasoso', 14.54, 19.22, true, 1, false);
+INSERT INTO public.planet VALUES (7, 'plutao', 'anao', 0.0022, 39.48, true, 1, false);
 
 
 --
 -- Data for Name: star; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
+INSERT INTO public.star VALUES (1, 'sol', 'ana g', 1, 5500, 1);
+INSERT INTO public.star VALUES (2, 'sirius', 'ana a', 2.1, 9800, 1);
+INSERT INTO public.star VALUES (3, 'betelgeuse', 'gigante vermelha', 20, 3500, 2);
+INSERT INTO public.star VALUES (4, 'proxima centauri', 'ana m', 0.12, 2800, 1);
+INSERT INTO public.star VALUES (5, 'alpha centauri a', 'ana g', 1.1, 5800, 1);
+INSERT INTO public.star VALUES (6, 'alpha centauri b', 'ana k', 0.9, 5200, 1);
+INSERT INTO public.star VALUES (7, 'vega', 'ana a', 2.1, 9600, 1);
+INSERT INTO public.star VALUES (8, 'polaris', 'supergigante', 5, 6000, 1);
+INSERT INTO public.star VALUES (9, 'aldebaran', 'supergigante', 1.7, 3900, 2);
+INSERT INTO public.star VALUES (10, 'antares', 'supergigante', 12, 3500, 1);
 
 
 --
 -- Name: constellation_constellation_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
-SELECT pg_catalog.setval('public.constellation_constellation_id_seq', 1, false);
+SELECT pg_catalog.setval('public.constellation_constellation_id_seq', 5, true);
 
 
 --
 -- Name: galaxy_galaxy_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
-SELECT pg_catalog.setval('public.galaxy_galaxy_id_seq', 1, false);
+SELECT pg_catalog.setval('public.galaxy_galaxy_id_seq', 10, true);
 
 
 --
 -- Name: moon_moon_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
-SELECT pg_catalog.setval('public.moon_moon_id_seq', 1, false);
+SELECT pg_catalog.setval('public.moon_moon_id_seq', 10, true);
 
 
 --
 -- Name: planet_planet_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
-SELECT pg_catalog.setval('public.planet_planet_id_seq', 1, false);
+SELECT pg_catalog.setval('public.planet_planet_id_seq', 7, true);
 
 
 --
 -- Name: star_star_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
-SELECT pg_catalog.setval('public.star_star_id_seq', 1, false);
+SELECT pg_catalog.setval('public.star_star_id_seq', 11, true);
 
 
 --
